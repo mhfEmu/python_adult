260a21c7bd14bda5cb4635ed6e1c6012ec68c88d
iMahdi <hasanmehedi699@gmail.com>
2021-06-21 18:01:43 +0600

    python_adult

---
+++ python_snake_game/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ("Courier", 21, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", False, align=ALIGN, font=FONT)
+
+    def increment_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", False, align=ALIGN, font=FONT)
